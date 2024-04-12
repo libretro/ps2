1011d6d861e80754aef2e6ec7d23bc27a6a536df
libretroadmin <reallibretroretroarch@gmail.com>
2024-04-12 08:33:59 +0200

    Remove unused members from WindowInfo

--- common/WindowInfo.h
+++ common/WindowInfo.h
@@ -19,18 +19,8 @@
 /// Contains the information required to create a graphics context in a window.
 struct WindowInfo
 {
-	/// Connection to the display server. On most platforms except X11/Wayland, this is implicit and null.
-	void* display_connection = nullptr;
-
-	/// Abstract handle to the window. This depends on the surface type.
-	void* window_handle = nullptr;
-
-	/// For platforms where a separate surface/layer handle is needed, it is stored here (e.g. MacOS).
-	void* surface_handle = nullptr;
-
 	/// Width of the surface in pixels.
 	u32 surface_width = 0;
-
 	/// Height of the surface in pixels.
 	u32 surface_height = 0;
 };
