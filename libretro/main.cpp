adfddc55787a5dc4e14465ec88f8ef73c00aa314
libretroadmin <reallibretroretroarch@gmail.com>
2024-04-30 02:34:20 +0200

    Merge options.cpp into libretro/main.cpp

--- libretro/main.cpp
+++ libretro/main.cpp
@@ -12,9 +12,9 @@
 #include <cstdint>
 #include <libretro.h>
 #include <string>
+#include <vector>
+#include <type_traits>
 #include <thread>
-
-#include "options.h"
 
 #include "GS.h"
 #include "svnrev.h"
@@ -62,40 +62,6 @@ static struct retro_perf_callback perf_cb;
 #define RETRO_PERFORMANCE_STOP(name)
 #endif
 
-namespace Options
-{
-static Option<std::string> bios("pcsx2_bios", "Bios"); // will be filled in retro_init()
-static Option<bool> fast_boot("pcsx2_fastboot", "Fast Boot", true);
-
-GfxOption<std::string> renderer("pcsx2_renderer", "Renderer", {
-		"Auto",
-		"OpenGL",
-#ifdef _WIN32
-		"D3D11", "D3D12",
-#endif
-#ifdef ENABLE_VULKAN
-		"Vulkan",
-#endif
-		"Software",
-		"Null"});
-
-GfxOption<int> upscale_multiplier("pcsx2_upscale_multiplier", "Internal Resolution",
-								  {
-								  {"1x Native (PS2)",         1},
-								  {"2x Native (~720p)",       2},
-								  {"3x Native (~1080p)",      3},
-								  {"4x Native (~1440p/2K)",   4},
-								  {"5x Native (~1800p/3K)",   5},
-								  {"6x Native (~2160p/4K)",   6},
-								  {"7x Native (~2520p)",      7},
-								  {"8x Native (~2880p/5K)",   8},
-								  {"9x Native (~3240p)",      9},
-								  {"10x Native (~3600p/6K)", 10},
-								  {"11x Native (~3960p)",    11},
-								  {"12x Native (~4320p/8K)", 12}   });
-//static GfxOption<int> sw_renderer_threads("pcsx2_sw_renderer_threads", "Software Renderer Threads", 2, 10);
-} // namespace Options
-
 retro_environment_t environ_cb;
 retro_video_refresh_t video_cb;
 struct retro_hw_render_callback hw_render;
@@ -111,6 +77,268 @@ static s16 snd_buffer[SOUND_BUFFER_SIZE];
 
 static VMState cpu_thread_state;
 static MemorySettingsInterface s_settings_interface;
+static std::thread cpu_thread;
+
+namespace Options
+{
+
+	enum Groups
+	{
+		OPTIONS_BASE,
+		OPTIONS_GFX,
+		OPTIONS_EMU,
+		OPTIONS_GROUPS_MAX
+	};
+
+	class OptionBase
+	{
+		public:
+			void SetDirty() { m_dirty = true; }
+			retro_variable getVariable() { return {m_id, m_options.c_str()}; }
+			virtual bool empty() = 0;
+
+		protected:
+			OptionBase(const char* id, const char* name, Groups group)
+				: m_id(id)
+				  , m_name(name)
+		{
+			m_options = m_name;
+			m_options.push_back(';');
+			Register(group);
+		}
+
+			const char* m_id;
+			const char* m_name;
+			bool m_dirty = true;
+			std::string m_options;
+
+		private:
+			void Register(Groups group);
+	};
+
+	template <typename T, Groups group = OPTIONS_BASE>
+		class Option : public OptionBase
+	{
+		static_assert(group < OPTIONS_GROUPS_MAX, "invalid option group index");
+		Option(Option&) = delete;
+		Option(Option&&) = delete;
+		Option& operator=(Option&) = delete;
+
+		public:
+		Option(const char* id, const char* name)
+			: OptionBase(id, name, group)
+		{
+		}
+
+		Option(const char* id, const char* name, T initial)
+			: OptionBase(id, name, group)
+		{
+			push_back(initial ? "enabled" : "disabled", initial);
+			push_back(!initial ? "enabled" : "disabled", !initial);
+		}
+
+		Option(const char* id, const char* name, std::initializer_list<std::pair<const char*, T>> list)
+			: OptionBase(id, name, group)
+		{
+			for (auto option : list)
+				push_back(option.first, option.second);
+		}
+
+		Option(const char* id, const char* name, std::initializer_list<const char*> list)
+			: OptionBase(id, name, group)
+		{
+			for (auto option : list)
+				push_back(option);
+		}
+
+		Option(const char* id, const char* name, T first, std::initializer_list<const char*> list)
+			: OptionBase(id, name, group)
+		{
+			for (auto option : list)
+				push_back(option, first + (int)m_list.size());
+		}
+
+		Option(const char* id, const char* name, T first, int count, int step = 1)
+			: OptionBase(id, name, group)
+		{
+			for (T i = first; i < first + count; i += step)
+				push_back(std::to_string(i), i);
+		}
+
+		void push_back(std::string option, T value)
+		{
+			if (m_list.empty())
+			{
+				m_options += std::string(" ") + option;
+				m_value = value;
+			}
+			else
+				m_options += std::string("|") + option;
+
+			m_list.push_back({option, value});
+		}
+
+		template <bool is_str = !std::is_integral<T>() && std::is_constructible<T, const char*>()>
+			void push_back(const char* option)
+			{
+				push_back(option, option);
+			}
+#if 0
+		template <>
+			void push_back<false>(const char* option)
+			{
+				if (m_list.empty())
+					push_back(option, 0);
+				else
+					push_back(option, m_list.back().second + 1);
+			}
+#endif
+		bool Updated()
+		{
+			if (m_dirty && !m_locked)
+			{
+				m_dirty = false;
+
+				retro_variable var{m_id};
+				T value = m_list.front().second;
+
+				if (environ_cb && environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
+				{
+					for (auto option : m_list)
+					{
+						if (option.first == var.value)
+						{
+							value = option.second;
+							break;
+						}
+					}
+				}
+
+				if (m_value != value)
+				{
+					m_value = value;
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		void UpdateAndLock()
+		{
+			m_locked = false;
+			Updated();
+			m_locked = true;
+		}
+
+		operator T()
+		{
+			Updated();
+			return m_value;
+		}
+
+		T Get()
+		{
+			return (T) * this;
+		}
+
+		template <typename S>
+			bool operator==(S value)
+			{
+				return (T) * this == value;
+			}
+
+		template <typename S>
+			bool operator!=(S value)
+			{
+				return (T) * this != value;
+			}
+
+		virtual bool empty() override { return m_list.empty(); }
+
+		private:
+		bool m_locked = false;
+		T m_value;
+		std::vector<std::pair<std::string, T>> m_list;
+	};
+
+	template <typename T>
+		using EmuOption = Option<T, OPTIONS_EMU>;
+	template <typename T>
+		using GfxOption = Option<T, OPTIONS_GFX>;
+
+	static std::vector<OptionBase*>& GetOptionGroup(Groups group)
+	{
+		/* this garentees that 'list' is constructed first before being accessed other global constructors.*/
+		static std::vector<OptionBase*> list[OPTIONS_GROUPS_MAX];
+		return list[group];
+	}
+
+	void OptionBase::Register(Groups group)
+	{
+		GetOptionGroup(group).push_back(this);
+	}
+
+	void SetVariables()
+	{
+		std::vector<retro_variable> vars;
+		for (int grp = 0; grp < OPTIONS_GROUPS_MAX; grp++)
+			for (OptionBase* option : GetOptionGroup((Groups)grp))
+				if (!option->empty())
+					vars.push_back(option->getVariable());
+
+		if (vars.empty())
+			return;
+
+		vars.push_back({});
+		environ_cb(RETRO_ENVIRONMENT_SET_VARIABLES, (void*)vars.data());
+	}
+
+	void CheckVariables()
+	{
+		bool updated = false;
+		if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE_UPDATE, &updated) && !updated)
+			return;
+
+		for (int grp = 0; grp < OPTIONS_GROUPS_MAX; grp++)
+			for (OptionBase* option : GetOptionGroup((Groups)grp))
+				option->SetDirty();
+	}
+
+	extern GfxOption<int> upscale_multiplier;
+	extern GfxOption<std::string> renderer;
+
+	static Option<std::string> bios("pcsx2_bios", "Bios"); // will be filled in retro_init()
+	static Option<bool> fast_boot("pcsx2_fastboot", "Fast Boot", true);
+
+	GfxOption<std::string> renderer("pcsx2_renderer", "Renderer", {
+			"Auto",
+			"OpenGL",
+#ifdef _WIN32
+			"D3D11", "D3D12",
+#endif
+#ifdef ENABLE_VULKAN
+			"Vulkan",
+#endif
+			"Software",
+			"Null"});
+
+	GfxOption<int> upscale_multiplier("pcsx2_upscale_multiplier", "Internal Resolution",
+			{
+			{"1x Native (PS2)",         1},
+			{"2x Native (~720p)",       2},
+			{"3x Native (~1080p)",      3},
+			{"4x Native (~1440p/2K)",   4},
+			{"5x Native (~1800p/3K)",   5},
+			{"6x Native (~2160p/4K)",   6},
+			{"7x Native (~2520p)",      7},
+			{"8x Native (~2880p/5K)",   8},
+			{"9x Native (~3240p)",      9},
+			{"10x Native (~3600p/6K)", 10},
+			{"11x Native (~3960p)",    11},
+			{"12x Native (~4320p/8K)", 12}   });
+	//static GfxOption<int> sw_renderer_threads("pcsx2_sw_renderer_threads", "Software Renderer Threads", 2, 10);
+} // namespace Options
 
 #ifdef ENABLE_VULKAN
 static retro_hw_render_interface_vulkan *vulkan;
@@ -119,8 +347,6 @@ void vk_libretro_init(VkInstance instance, VkPhysicalDevice gpu, const char **re
 void vk_libretro_shutdown(void);
 void vk_libretro_set_hwrender_interface(retro_hw_render_interface_vulkan *hw_render_interface);
 #endif
-
-static std::thread cpu_thread;
 
 static void cpu_thread_pause(void)
 {
