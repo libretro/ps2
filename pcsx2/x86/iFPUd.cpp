a793be0743bdd255c04a825cddb231851a51ce8d
libretroadmin <reallibretroretroarch@gmail.com>
2024-05-01 00:25:51 +0200

    Backport EE/JIT: Increase size of jump for FPU MUL with Full EE clamping

--- pcsx2/x86/iFPUd.cpp
+++ pcsx2/x86/iFPUd.cpp
@@ -410,7 +410,7 @@ void FPU_ADD_SUB(int tempd, int tempt) //tempd and tempt are overwritten, they a
 
 void FPU_MUL(int info, int regd, int sreg, int treg, bool acc)
 {
-	u8* endMul = nullptr;
+	u32* endMul = nullptr;
 
 	if (CHECK_FPUMULHACK)
 	{
@@ -431,7 +431,7 @@ void FPU_MUL(int info, int regd, int sreg, int treg, bool acc)
 
 		u8* noHack = JNZ8(0);
 			xMOVAPS(xRegisterSSE(regd), ptr128[result]);
-			endMul = JMP8(0);
+			endMul = JMP32(0);
 		x86SetJ8(noHack);
 	}
 
@@ -441,7 +441,7 @@ void FPU_MUL(int info, int regd, int sreg, int treg, bool acc)
 	xMOVSS(xRegisterSSE(regd), xRegisterSSE(sreg));
 
 	if (CHECK_FPUMULHACK)
-		x86SetJ8(endMul);
+		x86SetJ32(endMul);
 }
 
 //------------------------------------------------------------------
