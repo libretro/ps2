80ad0dd953587d42c85cfabd4cad8be19586cbb4
libretroadmin <reallibretroretroarch@gmail.com>
2024-05-06 14:23:56 +0200

    Slim down TextureDecompress.h

--- common/TextureDecompress.h
+++ common/TextureDecompress.h
@@ -89,64 +89,12 @@ public:
 		set_noclamp_rgba((uint8_t)sr, (uint8_t)sg, (uint8_t)sb, (uint8_t)sa);
 	}
 
-	inline color_rgba& set_noclamp_y(int y)
-	{
-		m_comps[0] = (uint8_t)y;
-		m_comps[1] = (uint8_t)y;
-		m_comps[2] = (uint8_t)y;
-		m_comps[3] = (uint8_t)255;
-		return *this;
-	}
-
 	inline color_rgba &set_noclamp_rgba(int sr, int sg, int sb, int sa)
 	{
 		m_comps[0] = (uint8_t)sr;
 		m_comps[1] = (uint8_t)sg;
 		m_comps[2] = (uint8_t)sb;
 		m_comps[3] = (uint8_t)sa;
-		return *this;
-	}
-
-	inline color_rgba &set(int y)
-	{
-		m_comps[0] = static_cast<uint8_t>(clamp<int>(y, 0, 255));
-		m_comps[1] = m_comps[0];
-		m_comps[2] = m_comps[0];
-		m_comps[3] = 255;
-		return *this;
-	}
-
-	inline color_rgba &set(int y, int na)
-	{
-		m_comps[0] = static_cast<uint8_t>(clamp<int>(y, 0, 255));
-		m_comps[1] = m_comps[0];
-		m_comps[2] = m_comps[0];
-		m_comps[3] = static_cast<uint8_t>(clamp<int>(na, 0, 255));
-		return *this;
-	}
-
-	inline color_rgba &set(int sr, int sg, int sb, int sa)
-	{
-		m_comps[0] = static_cast<uint8_t>(clamp<int>(sr, 0, 255));
-		m_comps[1] = static_cast<uint8_t>(clamp<int>(sg, 0, 255));
-		m_comps[2] = static_cast<uint8_t>(clamp<int>(sb, 0, 255));
-		m_comps[3] = static_cast<uint8_t>(clamp<int>(sa, 0, 255));
-		return *this;
-	}
-
-	inline color_rgba &set_rgb(int sr, int sg, int sb)
-	{
-		m_comps[0] = static_cast<uint8_t>(clamp<int>(sr, 0, 255));
-		m_comps[1] = static_cast<uint8_t>(clamp<int>(sg, 0, 255));
-		m_comps[2] = static_cast<uint8_t>(clamp<int>(sb, 0, 255));
-		return *this;
-	}
-
-	inline color_rgba &set_rgb(const color_rgba &other)
-	{
-		r = other.r;
-		g = other.g;
-		b = other.b;
 		return *this;
 	}
 
@@ -186,13 +134,6 @@ public:
 		}
 		return false;
 	}
-
-	inline int get_601_luma() const { return (19595U * m_comps[0] + 38470U * m_comps[1] + 7471U * m_comps[2] + 32768U) >> 16U; }
-	inline int get_709_luma() const { return (13938U * m_comps[0] + 46869U * m_comps[1] + 4729U * m_comps[2] + 32768U) >> 16U; } 
-	inline int get_luma(bool luma_601) const { return luma_601 ? get_601_luma() : get_709_luma(); }
-
-	static color_rgba comp_min(const color_rgba& a, const color_rgba& b) { return color_rgba(std::min(a[0], b[0]), std::min(a[1], b[1]), std::min(a[2], b[2]), std::min(a[3], b[3])); }
-	static color_rgba comp_max(const color_rgba& a, const color_rgba& b) { return color_rgba(std::max(a[0], b[0]), std::max(a[1], b[1]), std::max(a[2], b[2]), std::max(a[3], b[3])); }
 };
 
 bool unpack_bc7(const void *pBlock, color_rgba *pPixels);
