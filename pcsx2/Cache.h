ef43f3f7e2008c1d6aad31f854ecf09d31013edd
libretroadmin <reallibretroretroarch@gmail.com>
2024-08-16 04:21:16 +0200

    Merge vtlb.cpp into Cache.cpp

--- pcsx2/Cache.h
+++ pcsx2/Cache.h
@@ -17,14 +17,3 @@
 
 #include "Common.h"
 #include "common/SingleRegisterTypes.h"
-
-void writeCache8(u32 mem, u8 value);
-void writeCache16(u32 mem, u16 value);
-void writeCache32(u32 mem, u32 value);
-void writeCache64(u32 mem, const u64 value);
-void writeCache128(u32 mem, const mem128_t* value);
-u8 readCache8(u32 mem);
-u16 readCache16(u32 mem);
-u32 readCache32(u32 mem);
-u64 readCache64(u32 mem);
-RETURNS_R128 readCache128(u32 mem);
